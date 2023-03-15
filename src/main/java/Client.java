d9b4e9f0eae34130e952c59ac958563818a4649c
Marina Khapinskaya <marina_reic@mail.ru>
2023-03-15 23:13:49 +0700

    Added class Client

---
+++ src/main/java/Client.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    public static final String LOCALHOST = "127.0.0.1";
+
+    public static void main(String[] args) {
+
+        try (Socket socket = new Socket(LOCALHOST, Server.PORT);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             Scanner scanner = new Scanner(System.in);) {
+
+            out.println(scanner.nextLine());
+            System.out.println(in.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
